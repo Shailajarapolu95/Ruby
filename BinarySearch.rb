2422e0e498d1a5c472d70720560dd48cc45060f0
Shailajarapolu95 <103415604+Shailajarapolu95@users.noreply.github.com>
2022-04-20 10:56:52 +0530

    Update BinarySearch.rb

--- BinarySearch.rb
+++ BinarySearch.rb
@@ -14,7 +14,7 @@ def binary_search (a,key)  # a is the array and key is the value to be found
   return "Value not found in array...!!"
 end
 
-arr = [7, 6, 25, 19, 8, 14, 3, 16, 2, 0]
+arr = [2,4,6,7,8,10,15,18,20,26,43]
 puts "Enter key value..."
 key = gets.chomp.to_i
 p binary_search(arr, key)
